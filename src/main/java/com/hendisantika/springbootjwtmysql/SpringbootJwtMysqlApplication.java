47b8ac0380adcbcba4289d45c008a80dd5e46047
hendisantika <hendisantika@yahoo.co.id>
2019-02-25 06:40:17 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootjwtmysql/SpringbootJwtMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtMysqlApplication.class, args);
+    }
+
+}
