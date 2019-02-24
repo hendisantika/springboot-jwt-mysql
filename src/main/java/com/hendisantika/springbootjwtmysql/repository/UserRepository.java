a5c2e29c2f8a7b674961502929111b0f49bdfd1b
hendisantika <hendisantika@yahoo.co.id>
2019-02-25 06:54:21 +0700

    #11 - Add more method in User Repository

--- src/main/java/com/hendisantika/springbootjwtmysql/repository/UserRepository.java
+++ src/main/java/com/hendisantika/springbootjwtmysql/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.hendisantika.springbootjwtmysql.repository;
 
 import com.hendisantika.springbootjwtmysql.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +17,21 @@ import org.springframework.data.repository.CrudRepository;
  * Time: 06:42
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User getById(int id);
+
+    User getByEmailAddress(String emailAddress);
+
+    @Transactional
+    void deleteById(int userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into token (token, userId, expirationTime) values (token, userId, now() + interval 1 DAY )", nativeQuery = true)
+    void savePasswordToken(
+            @Param(value = "token") String token,
+            @Param(value = "userId") int userId
+    );
+
+    @Query(value = "select * from token where emailAddress=username", nativeQuery = true)
+    String getTokenByUserName(@Param(value = "username") String username);
 }
